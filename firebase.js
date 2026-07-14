57fe93175f1c8a896dcc37c7ac4e92f8d9f0e68b
MSTERPEGXSUS <anshulpanda54@gmail.com>
2026-07-14 19:29:49 +0100

    Initialize Firebase app and analytics

--- firebase.js
+++ firebase.js
@@ -1,1 +1,19 @@
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmS3JQazcHZFRtBejK-shUzVeHbwmDCsk",
+  authDomain: "cubes-62678.firebaseapp.com",
+  projectId: "cubes-62678",
+  storageBucket: "cubes-62678.firebasestorage.app",
+  messagingSenderId: "805108012327",
+  appId: "1:805108012327:web:a895ef82ca64bf58fbc502",
+  measurementId: "G-4EEXD4KKC4"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
