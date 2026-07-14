033d6d68f6fa85b1a07608006e96de5cbea9b899
MSTERPEGXSUS <anshulpanda54@gmail.com>
2026-07-14 19:31:25 +0100

    Add hover effect to cards in script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+console.log("Infinity Cubes V2 Loaded");
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+card.addEventListener("mouseenter",()=>{
+
+card.style.transform="translateY(-10px)";
+
+});
+
+card.addEventListener("mouseleave",()=>{
+
+card.style.transform="translateY(0px)";
+
+});
+
+});
